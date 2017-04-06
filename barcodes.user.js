8ebb0ff3107886736e62a630773f01e6dcd6776e
lsulliv <lsulliv@gmail.com>
2017-04-06 08:47:35 -0600

    added P3terPan
    added decoding for P3terPan

--- barcodes.user.js
+++ barcodes.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-barcodes@RoC
 // @name           IITC plugin: Replace barcode player names with friendly names on click
 // @category       Portal Info
-// @version        0.1.0.20170319.2243
+// @version        0.1.0.20170406.0847
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/ResistanceOfColorado/iitc/raw/master/barcodes.user.js
 // @downloadURL    https://github.com/ResistanceOfColorado/iitc/raw/master/barcodes.user.js
@@ -59,7 +59,8 @@ window.plugin.barcodes.nameMap = {
     "IIIIlIIllIlIlII": "BriaT",
     "IIlllIIIIlIlIll": "Gakby",
     "llllIIlllIllIII": "Trihga",
-    "IllllllIlIllIIl": "OzymandiasOne"
+    "IllllllIlIllIIl": "OzymandiasOne",
+    "IIIIllIIlllIIll": "P3terPan"
 };
 window.plugin.barcodes.barPatt = new RegExp("^[Il]{15}$");
 
