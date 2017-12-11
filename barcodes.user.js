222b1c1b533809ffdefdfb54ec5707c1923acc6a
lsulliv <lsulliv@gmail.com>
2017-12-11 13:28:32 -0700

    numerous updates to nameMap
    removed atsina due to name change to wyomountaineer, 
    updated tedly's new barcode, kept his old shadyglen map for reference

--- barcodes.user.js
+++ barcodes.user.js
@@ -2,7 +2,7 @@
 // @id             iitc-plugin-barcodes@RoC
 // @name           IITC plugin: Replace barcode player names with friendly names on click
 // @category       Portal Info
-// @version        0.1.0.20170524.2128
+// @version        0.1.0.20171211.1328
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/ResistanceOfColorado/iitc/raw/master/barcodes.user.js
 // @downloadURL    https://github.com/ResistanceOfColorado/iitc/raw/master/barcodes.user.js
@@ -62,7 +62,7 @@ window.plugin.barcodes.nameMap = {
     "IllllllIlIllIIl": "OzymandiasOne",
     "IIIIllIIlllIIll": "P3terPan",
     "IIlIlIllIlIlIII": "shadyglen",
-    "IlllIllIIlIIIl":  "atsina"
+    "IIlIlIllIlIlIIl": "tedly"
 };
 window.plugin.barcodes.barPatt = new RegExp("^[Il]{15}$");
 
